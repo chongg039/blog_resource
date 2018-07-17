67adaa94783663c80b4e0cf682541fd18f80af11
chongg039 <chongg039@gmail.com>
2018-07-17 11:42:36 +0800

    rm public/

--- public/js/function.js
+++
@@ -1,6 +0,0 @@
-(function (){
-    var pres = document.querySelectorAll("pre");
-    for (i = 0; i < pres.length; i++) {
-        pres[i].setAttribute("class", "prettyprint linenums");
-    }
-})();
